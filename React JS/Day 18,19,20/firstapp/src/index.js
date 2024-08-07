ada47c66e94525ad1f395ad92733931d5e62b186
Lasya M <119107777+lasya2411@users.noreply.github.com>
2024-08-07 15:28:11 +0530

    Add files via upload

---
+++ React JS/Day 18,19,20/firstapp/src/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<App/>); 
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
+
+
+/*
+const headtag =<h2>React is {15+5} times better when learning with interest </h2>
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(headtag); */
+
+/*
+const listex = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(listex); */
+
+/*
+const h1tag = <h1 clasName = "myclass" style={{color:"red"}}>Welcome to NYC</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(h1tag); */
+
+/*
+const x=50;
+const ternary = <h1 className="change" style={{color: "blue"}}>{(x) < 100 ? "You are correct" : "Oops! Better luck next time"}</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(ternary); */
+
+/*
+const x=50;
+let text = "Google";
+if(x<100) {
+  text = "Yahoo";
+}
+const myElement =<h1 className="change">{text}</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement); */
+
+/*
+function Mango()
+{
+  return <h1>Welcome to mango city of TN - Salem !</h1>;
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Mango />); */
+
+/*
+const myElement = (
+  <table class="center">
+    <tr>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Address</th>
+    </tr>
+    <tr>
+      <td>Mango</td>
+      <td>20</td>
+      <td>Salem</td>
+    </tr>
+    <tr>
+      <td>Apple</td>
+      <td>15</td>
+      <td>Kashmir</td>
+    </tr>
+    <tr>
+      <td>Orange</td>
+      <td>25</td>
+      <td>Darjelling</td>
+    </tr>
+  </table>
+  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement); */
+
+/*
+function Mango()
+{
+  var myStyle= {
+    fontSize: 50,
+    fontFamily:'arial',
+    color:'#003300'
+  }
+  return <h1 style={myStyle}>Welcome to Mango city</h1>
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Mango />); */const fruitList = ['Apple', 'Bananas', 'Grapes', 'Pineapple'];   
+/*const listItems = fruitList.map((myList)=>{   
+    return <li>{myList}</li>;   
+});   
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ol type="I"> {listItems} </ol>);
+root.render(<ol type="a"> {listItems} </ol>);
+root.render(<ul> {listItems} </ul> +  ); */
